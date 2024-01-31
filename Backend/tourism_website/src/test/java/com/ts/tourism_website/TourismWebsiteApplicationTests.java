05d42a5e8d11a288f8c7b9ba1f97e719103293b4
Karthikeya Konyala <konyalakarthikeya29@gmail.com>
2024-01-31 16:51:57 +0530

    Spring Boot installed

---
+++ Backend/tourism_website/src/test/java/com/ts/tourism_website/TourismWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.tourism_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
