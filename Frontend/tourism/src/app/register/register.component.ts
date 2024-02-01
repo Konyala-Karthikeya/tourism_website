d53ab4689d8130200ae9ab8d98eecc56e662cf3d
Yarragorlla Rajesh <yrajeshyrajeshyadav@gmail.com>
2024-02-01 18:04:30 +0530

    register component created

---
+++ Frontend/tourism/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  registerSubmit(regForm: any) {
+    console.log(regForm);
+  }
+
+}
