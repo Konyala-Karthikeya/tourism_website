fec57d2e6497f37d2462a6aadbd3d9323170df4e
ajay6228 <pasumarthyajaykumar@gmail.com>
2024-02-05 14:18:55 +0530

    created header component

---
+++ Frontend/tourism_website/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  cartItems: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
