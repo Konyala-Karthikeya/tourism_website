51c289598dfdf4a86b309e79e4e2f0be523bf38c
ajay6228 <pasumarthyajaykumar@gmail.com>
2024-02-05 15:17:41 +0530

    created header component

--- Frontend/tourism/src/app/app.module.ts
+++ Frontend/tourism/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
