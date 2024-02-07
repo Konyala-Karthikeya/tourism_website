44d56beac220faf334d67ddd85fb83bf88b73e6f
Karthikeya Konyala <konyalakarthikeya29@gmail.com>
2024-02-07 17:04:18 +0530

    Done with the Customer And updated some fields

---
+++ Backend/Tourism/src/main/java/com/dao/CustomerDao.java
@@ -0,0 +1,101 @@
+package com.dao;
+
+import java.util.List;
+import java.util.Random;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.model.Customer;
+import com.ts.config.TwilioConfig;
+
+@Service
+public class CustomerDao {
+
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Autowired
+	 private TwilioConfig twilioConfig;
+	
+	// Hash the password using BCrypt
+    private String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+    
+    public Customer addCustomer(Customer customer) {
+		String otp = generateOtp();
+		customer.setOtp(otp);
+		sendWelcomeEmail(customer);		// Send a welcome email
+		sendOtpViaTwilio(customer);		// Send OTP via Twilio
+		customer.setPassword(hashPassword(customer.getPassword()));
+		customer.setConfirmPassword(hashPassword(customer.getConfirmPassword()));
+		Customer savedCustomer = customerRepository.save(customer);   		// Save the employee
+		return savedCustomer;
+	}
+	
+    private void sendWelcomeEmail(Customer customer) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(customer.getEmailId());
+		message.setSubject("Welcome to our website");
+		message.setText("Dear " + customer.getUserName()+ ",\n\n"
+				+ "Thank you for registering ");
+
+		mailSender.send(message);
+	}
+    
+    private void sendOtpViaTwilio(Customer customer) {
+        String phoneNumber = customer.getPhoneNumber();
+        twilioConfig.sendOtp(phoneNumber, customer.getOtp());
+    }
+
+    private String generateOtp() {
+        Random random = new Random();
+        int otp = 100000 + random.nextInt(900000);
+        return String.valueOf(otp);
+    }
+    
+    public Customer getCustomerByEmail(String emailId) {
+		return customerRepository.findByEmail(emailId);
+	}
+    
+    public Customer customerLogin(String emailId, String password) {
+		Customer customer = customerRepository.findByEmail(emailId);
+		if (customer != null && BCrypt.checkpw(password, customer.getPassword())) {
+	        return customer;
+	    } else {
+	    	Customer product1 = new Customer("Customer Not Found to Login!!!","","","","","","","");
+			return product1;
+	    }
+	}
+    
+    public Customer getCustomerById(int customerId){
+    	return customerRepository.findById(customerId).orElse(null);
+    }
+    
+    public Customer getCustomerByName(String userName){
+    	return customerRepository.findByName(userName);
+    }
+    
+    public List<Customer> getCustomers() {
+		return customerRepository.findAll();
+	}
+
+	public Customer updateCustomer(Customer customer) {
+		if(customerRepository.findById(customer.getCustomerId()) != null)return customerRepository.save(customer);
+		else {
+			Customer product1 = new Customer("Customer Not Found to Update!!!"," "," "," "," "," "," "," ");
+			return product1;
+		}
+	}
+
+	public void deleteCustomerById(int customerId) {
+		customerRepository.deleteById(customerId);
+	}
+}
